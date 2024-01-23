09bef967ecd2c0c3486786d6a8f73255d3ae10fc
TikoMan <tiko.manukyan@inbox.ru>
2024-01-24 00:07:58 +0400

    add update validate schema for users

--- schema/usersSchema.js
+++ schema/usersSchema.js
@@ -13,6 +13,13 @@ export default {
     password: Joi.string().required(),
   }),
 
+  update: Joi.object({
+    firstName: Joi.string().required(),
+    lastName: Joi.string().required(),
+    email: Joi.string().trim().email().required(),
+    password: Joi.string().required(),
+  }),
+
   list: Joi.object({
     page: Joi.number().min(1),
     limit: Joi.number().min(1).max(100),
