dd6e36862de63bde7d318bf81acec682cc487594
TikoMan <tiko.manukyan@inbox.ru>
2024-01-23 23:26:12 +0400

    add list schema

--- schema/usersSchema.js
+++ schema/usersSchema.js
@@ -7,8 +7,15 @@ export default {
     email: Joi.string().trim().email().required(),
     password: Joi.string().required(),
   }),
+
   login: Joi.object({
     email: Joi.string().trim().email().required(),
     password: Joi.string().required(),
+  }),
+
+  list: Joi.object({
+    page: Joi.number().min(1),
+    limit: Joi.number().min(1).max(100),
+    s: Joi.string().trim().allow(''),
   }),
 };
