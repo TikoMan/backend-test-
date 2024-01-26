433b3e403e2f0d1b803cb1ab06bc22186cf47a7a
TikoMan <tiko.manukyan@inbox.ru>
2024-01-26 19:17:09 +0400

    add update validation schema for comments

--- schema/commentsSchema.js
+++ schema/commentsSchema.js
@@ -5,4 +5,9 @@ export default {
     blogId: Joi.number().min(1).required(),
     text: Joi.string().trim().required(),
   }),
+
+  update: Joi.object({
+    id: Joi.number().min(1).required(),
+    text: Joi.string().trim().required(),
+  }),
 };
