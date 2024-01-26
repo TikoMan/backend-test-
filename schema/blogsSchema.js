2b324087aef7aedf1838c8948fd89f397d49ded6
TikoMan <tiko.manukyan@inbox.ru>
2024-01-26 22:37:01 +0400

    add show validate schema for blogs

--- schema/blogsSchema.js
+++ schema/blogsSchema.js
@@ -16,4 +16,9 @@ export default {
     page: Joi.number().min(1),
     limit: Joi.number().min(1).max(100),
   }),
+
+  show: Joi.object({
+    page: Joi.number().min(1),
+    limit: Joi.number().min(1).max(100),
+  }),
 };
